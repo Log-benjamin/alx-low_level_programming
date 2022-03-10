ca1359626f61b0328d94f988cf251f76998dbc5a
binyam tegene <binyampowerhc@gmail.com>
2022-03-10 14:59:59 -0800

    Have done Hello printf task

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -2,10 +2,10 @@
 /**
  * main - Entry point
  * Hello,print f task
- * Return:0 when execution end 
+ * Return: 0 when execution end 
  */
 int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art,\n");
+	printf( "with proper grammar, but the outcome is a piece of art,\n" );
 	return(0);
 }
