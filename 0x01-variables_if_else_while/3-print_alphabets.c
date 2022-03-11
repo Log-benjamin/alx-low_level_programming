cd41d193756c7134f254e82394bfc9e18a243699
binyam tegene <binyampowerhc@gmail.com>
2022-03-11 13:30:26 -0800

    Have done the alphABET task

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -8,15 +8,14 @@ int main(void)
 {
 	char L;
 
-	for (L = 'a' ; L <='z' ; L++)
+	for (L = 'a' ; L <= 'z' ; L++)
 	{
 		putchar(L);
 	}
-	for (L = 'A' ; L <='Z' ; L++)
+	for (L = 'A' ; L <= 'Z' ; L++)
 	{
 		putchar(L);
 	}
 	putchar('\n');
 	return (0);
 }
-	       	
