ade2aa980c90ae5c5c05ea872f07665f5ee8c381
binyam tegene <binyampowerhc@gmail.com>
2022-03-14 05:12:37 -0700

    Have done the Software is eating the World task

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i;
+	int j;
+	int x;
+	int y;
+
+	for (i = 48; i <= 57; i++)
+	{
+		for (j = 48; j <= 56; j++)
+		{
+			for (x = 48; x <= 57; x++)
+			{
+				for (y = 48; y <= 57; y++)
+				{
+					if (i == x && j == y)
+					{
+						continue;
+					}
+					putchar(i);
+					putchar(j);
+					putchar(' ');
+					putchar(x);
+					putchar(y);
+					if ( i == 57 && j == 56 && x == 57 && y == 57)
+					{
+						break;
+					}
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
