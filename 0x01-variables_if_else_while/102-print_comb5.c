a4284bd854aeed57760afc3aae1259fcd514120e
binyam tegene <binyampowerhc@gmail.com>
2022-03-14 14:42:08 -0700

    Have done  Software is eating the World task

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -10,34 +10,39 @@ int main(void)
 	int j;
 	int x;
 	int y;
+	int first;
+	int last;
 
-	for (i = 48; i <= 57; i++)
-	{
-		for (j = 48; j <= 56; j++)
-		{
-			for (x = 48; x <= 57; x++)
-			{
-				for (y = 48; y  <= 57; y++)
-				{
-					if ((i + j > x + y) || (x < i) || ( i ==x && j == y))
-				{
-					continue;
-				}
-					putchar(i);
-					putchar(j);
-					putchar(' ');
-					putchar(x);
-					putchar(y);
-					if (i == 57 && j == 56 && x == 57 && y == 57)
-					{
-						break;
-					}
-					putchar(',');
-					putchar(' ');
-				}
-			}
-		}
-	}
-	putchar('\n');
-	return (0);
+for (i = 48; i <= 57; i++)
+{
+for (j = 48; j <= 56; j++)
+{
+for (x = 48; x <= 57; x++)
+{
+for (y = 48; y  <= 57; y++)
+{
+	first = i * 10 + j;
+	last = x * 10 + y;
+if ( first >= last )
+{
+continue;
 }
+putchar(i);
+putchar(j);
+putchar(' ');
+putchar(x);
+putchar(y);
+if (i == 57 && j == 56 && x == 57 && y == 57)
+{
+break;
+}
+putchar(',');
+putchar(' ');
+}
+}
+}
+}
+putchar('\n');
+return (0);
+}
+
