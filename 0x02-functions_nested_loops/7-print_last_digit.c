fcdf69d0ff5591f395eee14f8f20c683f7470d1a
binyam tegene <binyampowerhc@gmail.com>
2022-03-15 14:53:04 -0700

    Have done the There are only 3 colors task

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+ * print_last_digit- prints the last digit of a number.
+ * @z: passed integer
+ * Return: will be returned an integer value
+ */
+int print_last_digit(int z)
+{
+	return (z % 10);
+}
