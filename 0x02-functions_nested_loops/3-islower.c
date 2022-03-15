d1acc512853f14f7015369e0951fd2115dfa7c2d
binyam tegene <binyampowerhc@gmail.com>
2022-03-15 14:01:34 -0700

    Have done the islower task

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,6 +1,8 @@
 #include "main.h"
 /**
  * _islower -  checks for lowercase character.
+ * @c: takes in a character
+ * Return: 0 for uppercase, 1 for lowercase
  */
 int _islower(int c)
 {
@@ -8,7 +10,7 @@ int _islower(int c)
 
 	for (i = 'a'; i <= 'z'; i++)
 	{
-		if ( i == c)
+		if (i == c)
 			return (1);
 	}
 	return (0);
