a8f1a4b68182e985fe85d63a77e5e2e044c25328
binyam tegene <binyampowerhc@gmail.com>
2022-03-16 12:31:12 -0700

    Have done the  In computer class, the first assignment was to write a program to print the first 100 Fibonacci numbers task

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -5,25 +5,35 @@
  */
 int main(void)
 {
-	int counter = 2;
-	float a = 1;
-	float b = a + 1;
-	float c = a + b;
-	
-	printf("%.0f, ", a);
-	printf("%.0f, ", b);
-	while (counter < 98)
+	unsigned long int i, i0, i1, j, j0, j1, e, e0, e1, y, k;
+
+	i = 1;
+	j = 2;
+	e = i + j;
+
+	printf("%lu, ", i);
+	printf("%lu, ", j);
+	for (y = 3; y < 89; y++)
 	{
-		counter++;
-		printf("%.0f", c);
-		a = b;
-		b = c;
-		c = a + b;
-		if (counter < 98)
-		{
-			printf(", ");
-		}
+		printf("%lu, ", e);
+		i = j;
+		j = e;
+		e = i + j;
 	}
-	printf("\n");
+	j0 = j / 1000000000;
+	j1 = j % 1000000000;
+	e0 = e / 1000000000;
+	e1 = e % 1000000000;
+	for (k = 89; k < 98; k++)
+	{
+		printf("%lu%lu, ", e0, e1);
+		i0 = j0;
+		i1 = j1;
+		j0 = e0;
+		j1 = e1;
+		e0 = i0 + j0 + ((i1 + j1) / 1000000000);
+		e1 = (i1 + j1) % 1000000000;
+	}
+	printf("%lu%lu\n", e0, e1);
 	return (0);
 }
