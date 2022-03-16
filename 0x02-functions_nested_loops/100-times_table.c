423d885d97fee039fb22291aa7f4ca28b57594ea
binyam tegene <binyampowerhc@gmail.com>
2022-03-16 01:28:14 -0700

    Have Done the The World looks like a multiplication-table task

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * print_times_table - a function that prints the n times table, starting with 0
+ * @n: takes in an integer 
+ * void:have no return value
+ */
+void print_times_table(int n)
+{
+	int i;
+	int j;
+	
+	i = 0;
+
+	while (i <= n)
+	{
+		for (j = 0; j <= n; j++)
+		{
+			_putchar(i * j + '\0');
+		}
+			-putchar('\n');
+		i++;
+	}
+}
