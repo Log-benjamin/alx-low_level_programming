83efa21772d56981c3ae1d4b5dd779c602380d33
binyam tegene <binyampowerhc@gmail.com>
2022-03-16 10:20:47 -0700

    Have done The World looks like a multiplication-table task

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -8,16 +8,23 @@ void print_times_table(int n)
 {
 	int i;
 	int j;
-	
+	int k;
+
 	i = 0;
 
+	if (n > 15 || n < 0)
+		return;
+	else
+	{
 	while (i <= n)
 	{
 		for (j = 0; j <= n; j++)
 		{
-			_putcha((i * j) + '0');
+			k = i * j;
+			_putchar(k + '0');
 		}
 			_putchar('\n');
 		i++;
 	}
+}
 }
