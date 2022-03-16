71ac5d25fc8ff7af7315d93dd40e3d0c528e2087
binyam tegene <binyampowerhc@gmail.com>
2022-03-16 11:13:09 -0700

    Have done The World looks like a multiplication-table task

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,7 +1,7 @@
 #include "main.h"
 /**
- * print_times_table - a function that prints the n times table, starting with 0
- * @n: takes in an integer 
+ * print_times_table - a function prints n times table,
+ * @n: takes in an integer
  * void:have no return value
  */
 void print_times_table(int n)
@@ -10,28 +10,42 @@ void print_times_table(int n)
 	int j;
 	int k;
 
-	i = 0;
-
 	if (n > 15 || n < 0)
 		return;
-	else
-	{
-	while (i <= n)
+	for (i = 0; i <= n; i++)
 	{
 		for (j = 0; j <= n; j++)
 		{
 			k = i * j;
-			_putchar(k + '0');
-			if (j < n)
+			if (j == 0)
 			{
-			_putchar(',');
-			_putchar(' ');
-			_putchar(' ');
-			_putchar(' ');
+				_putchar(k + '0');
+			}
+			else if (k <= 9)
+			{
+				_putchar(',');
+				_putchar(' ');
+				_putchar(' ');
+				_putchar(' ');
+				_putchar(k + '0');
+			}
+			else if (k >= 10 && k < 100)
+			{
+				_putchar(',');
+				_putchar(' ');
+				_putchar(' ');
+				_putchar(k / 10 + '0');
+				_putchar(k % 10 + '0');
+			}
+			else
+			{
+				_putchar(',');
+				_putchar(' ');
+				_putchar((k / 100) + '0');
+				_putchar(k / 10 % 10 + '0');
+				_putchar(k % 10 + '0');
 			}
 		}
-			_putchar('\n');
-		i++;
-	}
+		_putchar('\n');
 }
 }
