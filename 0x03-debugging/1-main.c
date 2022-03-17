c449e9ca45f6a367fbcc9e0d7ae5c011040f6c9d
binyam tegene <binyampowerhc@gmail.com>
2022-03-17 12:39:26 -0700

    Have done the Like, comment, subscribe task

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -8,7 +8,9 @@ int main(void)
 	int i;
 
 	printf("Infinite loop incoming :(\n");
+
 	i = 0;
+
 /*
 * while (i < 10)
 * {
@@ -16,6 +18,8 @@ int main(void)
 * putchar(i);
 * }
 */
+
 	printf("Infinite loop avoided! \\o/\n");
+
 	return (0);
 }
