cad2f49d1da21c67b66e78495af27dbd88cdfe0d
binyam tegene <binyampowerhc@gmail.com>
2022-03-17 11:37:36 -0700

    Have done the Like comment subscribe

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -1,10 +1,8 @@
 #include <stdio.h>
-
 /**
  * main - causes an infinite loop
  * Return: 0
  */
-
 int main(void)
 {
 	int i;
@@ -19,6 +17,5 @@ int main(void)
  *}
  */
 	printf("Infinte loop avoided! \\o/\n");
-
 	return (0);
 }
