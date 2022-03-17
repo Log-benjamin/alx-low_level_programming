ef3ea3009d1a303b8e657f5a0a0c8bd4b1eee9ff
binyam tegene <binyampowerhc@gmail.com>
2022-03-17 12:05:59 -0700

    Have done the Like comment subscribe

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -6,17 +6,16 @@
 int main(void)
 {
 	int i;
-
+	
 	printf("Infinite loop incoming :(\n");
+	
 	i = 0;
 /*
- *
  * while (i < 10)
  * {
  * Infinite loop - no increment in variable
  * putchar(i);
  * }
- *
  */
 	printf("Infinite loop avoided! \\o/\n");
 	return (0);
