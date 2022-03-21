0b67ef95b266f8fd609a7bf4fc1c88d2ee169f4c
binyam tegene <binyampowerhc@gmail.com>
2022-03-21 13:19:35 -0700

    Have done Task 4. I can only go one way

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * _puts - prints a string, followed by a new line, to stdout.
+ * @str: input string
+ */
+void print_rev(char *s)
+{
+	int z;
+
+	for (z = 0; s[z] != '\0'; z++)
+	{
+	}
+	z = z - 1;
+       while (z >= 0)
+       {
+       	_putchar(s[z]);
+	z--;
+       }
+	_putchar('\n');       
+}
