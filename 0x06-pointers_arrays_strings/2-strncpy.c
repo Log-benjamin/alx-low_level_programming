c120368e2c9b9c73950132f6a2461a0314f9efc6
binyam tegene <binyampowerhc@gmail.com>
2022-03-23 11:11:45 -0700

    Have done Task 2 strncpy

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -10,16 +10,9 @@ char *_strncpy(char *dest, char *src, int n)
 {
 	int k = 0;
 
-	while ((k <= n) && (src[k] != '\0'))
+	while (k < n)
 	{
-		if (k == n)
-		{
-			dest[k] = '\0';
-		}
-		else
-		{
 		dest[k] = src[k];
-		}
 		k++;
 	}
 	return (dest);
