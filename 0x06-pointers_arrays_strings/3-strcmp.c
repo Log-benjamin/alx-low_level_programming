2132027276eb145c37d50620a721e0be50b7cb4b
binyam tegene <binyampowerhc@gmail.com>
2022-03-23 12:39:21 -0700

    Have done Task 3 strcmp

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -7,23 +7,17 @@
  */
 int _strcmp(char *s1, char *s2)
 {
-	int i;
-	int j;
-	int sum1 = 0;
-	int sum2 = 0;
-
-	for (i = 0; s1[i] != '\0'; i++)
+	while ((*s1 != '\0' && *s2 != '\0') && (*s1 == *s2))
 	{
-		sum1 = sum1 + ((int)(s1[i] - '0'));
+		s1++;
+		s2++;
 	}
-	for (j = 0; s2[j] != '\0'; j++)
+	if (*s1 == *s2)
 	{
-		sum2 = sum2  + ((int)(s2[j] - '0'));
-	}
-	if (sum1 > sum2)
-		return (15);
-	else if (sum1 == sum2)
 		return (0);
+	}
 	else
-		return (-15);
+	{
+		return (*s1 - *s2);
+	}
 }
