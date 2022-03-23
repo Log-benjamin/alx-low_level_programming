84fe72bdff3e8dfe63d25c845db793eccf9c047a
binyam tegene <binyampowerhc@gmail.com>
2022-03-23 09:19:12 -0700

    Have done Task 0 strcat

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,33 @@
+#include "main.h"
+/**
+ * _strcat - concatenates two strings.
+ * @dest: pointer input string.
+ * @src: pointer input string
+ */
+char *_strcat(char *dest, char *src)
+{
+	int i;
+	int j;
+	int k = 0;
+
+	for (i = 0; dest[i] != '\0'; i++)
+	{
+	}
+	for (j = 0; src[j] != '\0'; j++)
+	{
+	}
+	while ((i <= i + j) && (k <= j))
+	{
+		if (k == j)
+		{
+			dest[i] = '\0';
+		}
+		else
+		{
+		dest[i] = src[k];
+		}
+		i++;
+		k++;
+	}
+	return (dest);
+}
---
+++ 0x06-pointers_arrays_strings/README.md
@@ -0,0 +1,7 @@
+Task 0  concatenates two strings.
+Task 1 is a function that concatenates two strings.
+Task 2 is function that copies a string.
+Task 3 is a function that compares two strings.
+Task 4 is a function that reverses the content of an array of integers.
+Task 5 is a function that changes all lowercase letters of a string to uppercaseTask 6 is a function that capitalizes all words of a string.
+Task 7 is a function that encodes a string into 1337.
