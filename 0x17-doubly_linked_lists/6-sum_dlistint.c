d888f881a9eb74f20b16a9c741eeead24ff7affc
binyam <binyampowerhc@gmail.com>
2022-06-10 16:31:57 +0300

    Have Done Task 6

--- 0x17-doubly_linked_lists/6-sum_dlistint.c
+++ 0x17-doubly_linked_lists/6-sum_dlistint.c
@@ -8,15 +8,14 @@
 int sum_dlistint(dlistint_t *head)
 {
 	size_t res = 0;
-
+	
 	if (head == NULL)
 		return (0);
-
+	
 	while (head != NULL)
 	{
 		res += head->n;
 		head = head->next;
 	}
-	
 	return (res);
 }
