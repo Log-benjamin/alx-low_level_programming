38a4136d231c2caeccdda1b7958464690def15b9
binyam <binyampowerhc@gmail.com>
2022-06-14 10:36:03 +0300

    Have done Task 1

--- 0x17-doubly_linked_lists/1-dlistint_len.c
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -1,19 +1,14 @@
 #include "lists.h"
 
 /**
- * dlistint_len -counts the number of nodes
+ * dlistint_len - counts the number of nodes
  * @h: head of node
- * REturn: number of nodes
+ * Return: number of nodes
  */
 size_t dlistint_len(const dlistint_t *h)
 {
-	size_t count = 0;
-
-	while (h != NULL)
-	{
-		h = h->next;
-		count++;
-	}
-
-	return (count);
+	if (h == NULL)
+		return (0);
+	else
+		return (dlistint_len(h->next) +1);
 }
